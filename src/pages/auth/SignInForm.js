c77db2cf0fb15f6f4f1c552ca2e0e60629325fc0
vanderpatrick <patrick123321123@icloud.com>
2022-10-21 13:23:04 +0000

    create User context for naVbar

--- src/pages/auth/SignInForm.js
+++ src/pages/auth/SignInForm.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
@@ -8,23 +8,96 @@ import Row from "react-bootstrap/Row";
 import Image from "react-bootstrap/Image";
 import Container from "react-bootstrap/Container";
 
-import { Link } from "react-router-dom";
-
+import { Link, useHistory } from "react-router-dom";
+import { SetCurrentUserContext } from "../../App";
 import styles from "../../styles/SigninUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../app.module.css";
+import axios from "axios";
 
 function SignInForm() {
-//   Add your component logic here
+  const setCurrentUser = useContext(SetCurrentUserContext)
+
+  const [singInData, setSignInData] = useState({
+    username: "",
+    passaword: "",
+  });
+
+  const { username, password } = singInData;
+  const [errors, setErrors] = useState({});
+
+  const handleChange = (event) => {
+    setSignInData({
+      ...singInData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const submitHandle = async (event) => {
+    event.preventDefault();
+    try {
+      const {data} = await axios.post("/dj-rest-auth/login/", singInData);
+      setCurrentUser(data.user)
+      history.push("/");
+    } catch (err) {
+      setErrors(err.response?.data);
+    }
+  };
+  const history = useHistory();
 
   return (
     <Row className={styles.Row}>
       <Col className="my-auto p-0 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign in</h1>
-          
-          
+          <Form onSubmit={submitHandle}>
+            <Form.Group controlId="username">
+              <Form.Label className="d-none">Username</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Username"
+                name="username"
+                className={styles.Input}
+                value={username}
+                onChange={handleChange}
+              />
+            </Form.Group>
 
+            {errors.username?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+
+            <Form.Group controlId="password">
+              <Form.Label className="d-none">Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+                name="password"
+                className={styles.Input}
+                value={password}
+                onChange={handleChange}
+              />
+            </Form.Group>
+
+            {errors.password?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))}
+            <Button
+              className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}
+              type="submit"
+            >
+              Sign In
+            </Button>
+            {errors.non_field_errors?.map((message, idx) => (
+              <Alert variant="warning" className="mt-3" key={idx}>
+                {message}
+              </Alert>
+            ))}
+          </Form>
         </Container>
         <Container className={`mt-3 ${appStyles.Content}`}>
           <Link className={styles.Link} to="/signup">
