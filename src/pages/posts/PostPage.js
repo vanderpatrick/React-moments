e1a62d1863751d0cf9780ce7d7b13ce9fdc2654b
vanderpatrick <patrick123321123@icloud.com>
2022-10-25 12:00:35 +0000

    Add infinit scrolling for comments

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -12,6 +12,9 @@ import Comment from "../comments/Comment";
 
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../context/CurrentUserContext";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
 
 function PostPage() {
   const { id } = useParams();
@@ -56,9 +59,20 @@ function PostPage() {
             "Comments"
           ) : null}
           {comments.results.length ? (
-            comments.results.map((comment) => (
-              <Comment key={comment.id} {...comment} />
-            ))
+            <InfiniteScroll
+              children={comments.results.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  {...comment}
+                  setPost={setPost}
+                  setComments={setComments}
+                />
+              ))}
+              dataLength={comments.results.length}
+              loader={<Asset />}
+              hasMore={!!comments.next}
+              next={()=> fetchMoreData(comments,setComments)}
+            />
           ) : currentUser ? (
             <span>No comments yet, be the first to comment!</span>
           ) : (
